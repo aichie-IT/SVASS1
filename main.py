1c6e7c23300b2672f7283c3ca8a0836f39b62ce6
aichie-IT <s22a0051@siswa.umk.edu.my>
2025-10-27 01:23:23 +0800

    Fix metric display for PLO values in main.py

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ URL = 'https://raw.githubusercontent.com/aichie-IT/SVASS1/refs/heads/main/arts_f
 
 col1, col2, col3, col4 = st.columns(4)
    
-    col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
-    col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
-    col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
-    col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
+col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
+col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
+col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
+col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
+
 # Set Streamlit page configuration
 st.set_page_config(
     page_title="Scientific Visualization",
