0e87bbfb617d5fbdc2400da60bd01b4ad8bb1a07
aichie-IT <s22a0051@siswa.umk.edu.my>
2025-10-27 01:21:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ import plotly.graph_objects as go
 # Define the URL for the data source
 URL = 'https://raw.githubusercontent.com/aichie-IT/SVASS1/refs/heads/main/arts_faculty_data.csv'
 
+col1, col2, col3, col4 = st.columns(4)
+   
+    col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
+    col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
+    col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
+    col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
 # Set Streamlit page configuration
 st.set_page_config(
     page_title="Scientific Visualization",
@@ -32,14 +38,6 @@ arts_df = load_data(URL)
 if arts_df.empty:
     st.stop() # Stop execution if data loading failed
     
-col1, col2, col3, col4 = st.columns(4)
-   
-    col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
-    col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
-    col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
-    col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
-
-
 # --- 2. Data Cleaning and Preparation for Visualizations ---
 
 # Coerce GPA columns to numeric for calculation (as done in the original code)
